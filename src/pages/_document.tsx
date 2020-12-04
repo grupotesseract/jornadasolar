f89c1f0e1f7256ec637c8611c55d7bee1375e976
kellynvd <kellynvd@gmail.com>
2020-12-04 15:19:22 -0300

    style(theme): Ajusta cores e adiciona fonte

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -62,6 +62,11 @@ export default class extends Document {
             href="/icons/apple-touch-icon.png"
           />
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
--- theme.ts
+++ theme.ts
@@ -2,15 +2,28 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
-      main: '#FFFFFF'
+      main: '#F7C92A'
     },
     secondary: {
-      main: '#F7C92A'
+      main: '#000000'
     },
     background: {
       default: '#000000'
+    },
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#E0E0E0'
+    },
+    action: {
+      active: '#F7C92A',
+      focus: '#F7C92A',
+      hover: '#F7C92A'
     }
+  },
+  typography: {
+    fontFamily: ['Nunito Sans', 'sans-serif'].join(',')
   }
 })
 
