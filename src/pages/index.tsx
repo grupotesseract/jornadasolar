5b41b6949c07b9f1e150f4bb72df85249176ee3a
kellynvd <kellynvd@gmail.com>
2020-12-09 09:24:58 -0300

    feat: Adiciona tela inicial de apresentação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,71 @@
 import React, { FC } from 'react'
-import { Box, Container, Typography } from '@material-ui/core/'
+import { Box, Container, Typography } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import Button from '../components/Button'
 import Link from 'next/link'
 
-const Home: FC = () => (
-  <Container maxWidth="sm">
-    <Box my={4}>
-      <Typography variant="h4" component="h1" color="primary">
-        Jornada Solar
-      </Typography>
-
-      <Link href="/cadastro">
-        <a>Cadastro</a>
-      </Link>
-    </Box>
-  </Container>
+const useStyles = makeStyles(() =>
+  createStyles({
+    titulo: {
+      fontSize: 20,
+      fontWeight: 700
+    },
+    texto: {
+      maxWidth: 300,
+      fontSize: 20,
+      lineHeight: '27px',
+      textAlign: 'center'
+    }
+  })
 )
+
+const Home: FC = () => {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="xs">
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Box mt={2} alignSelf="center">
+          <Typography color="primary" className={classes.titulo}>
+            Jornada Solar
+          </Typography>
+        </Box>
+
+        <Box
+          mt={2}
+          flexGrow="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {/* TODO: Substituir icon por ilustração */}
+          <img src="/icons/icon-512x512.png" width="154px" height="154px" />
+
+          <Typography className={classes.texto}>
+            Embarque em uma jornada de autoconhecimento e monitore seus hábitos
+            e emoções
+          </Typography>
+        </Box>
+
+        <Box mb={2} alignSelf="center">
+          <Link href="/cadastro" passHref>
+            <Button variant="contained" color="primary">
+              Começar Jornada
+            </Button>
+          </Link>
+        </Box>
+
+        <Box mb={3} alignSelf="center">
+          <Link href="/login" passHref>
+            <Button variant="outlined" color="primary">
+              Já tenho cadastro
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
 
 export default Home
