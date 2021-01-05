5688bf9dd0e0e61c3587a5152b1e0c597aef3186
kellynvd <kellynvd@gmail.com>
2021-01-05 10:35:36 -0300

    feat: Adiciona componte TextArea estilizado

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { TextField, TextFieldProps } from '@material-ui/core'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    underline: {
+      '&&&:before': {
+        borderBottom: 'none'
+      },
+      '&&:after': {
+        borderBottom: 'none'
+      }
+    }
+  })
+)
+
+const TextArea: FC<TextFieldProps> = ({ onChange, id, value }) => {
+  const classes = useStyles()
+
+  return (
+    <TextField
+      onChange={onChange}
+      fullWidth
+      id={id}
+      multiline
+      value={value}
+      InputProps={{ classes }}
+    />
+  )
+}
+
+export default TextArea
