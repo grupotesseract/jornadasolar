2be3735d82849da32f48ec430a010277da89017c
kellynvd <kellynvd@gmail.com>
2021-01-14 17:39:12 -0300

    feat: Adiciona gráfico de hábitos
    - Adiciona componente GraficoBase
    - Adiciona componente GraficoHabitos
    - Ajusta GraficoSentimentos para usar GraficoBase
    - Adiciona GraficoHabitos na página graficos

---
+++ src/components/graficos/GraficoHabitos.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+import { IDiario } from '../../services/GetUserDiarioByDate'
+import { gruposDeHabitos } from '../diario/Habito'
+import BaseGraficos from './GraficoBase'
+
+interface IProps {
+  diarios: Array<IDiario>
+  mesAtual: Date
+}
+
+const GraficoHabitos: FC<IProps> = ({ diarios, mesAtual }) => {
+  const habitosDoMes = diarios
+    ?.map(diario => diario.gruposDeHabitos)
+    .flat()
+    .map(grupo => grupo.habitos)
+    .flat()
+    .filter(habito => Boolean(habito))
+
+  const listaDeHabitosComEmojis = gruposDeHabitos
+    .map(grupo => grupo.habitos)
+    .flat()
+
+  return (
+    <BaseGraficos
+      registrosDoMes={habitosDoMes}
+      mesAtual={mesAtual}
+      listaComEmojis={listaDeHabitosComEmojis}
+      titulo="Esses hábitos tiveram o maior impacto em sua vida neste mês:"
+    />
+  )
+}
+
+export default GraficoHabitos
--- src/components/graficos/GraficoSentimentos.tsx
+++ src/components/graficos/GraficoSentimentos.tsx
@@ -1,22 +1,7 @@
-import { Box, Typography } from '@material-ui/core'
-import { getDaysInMonth } from 'date-fns'
 import React, { FC } from 'react'
-import { makeStyles, createStyles } from '@material-ui/core/styles'
-import ProgressBar from '../ProgressBar'
-import { sentimentos as listaDeSentimentos } from '../diario/Sentimento'
+import { sentimentos as listaDeSentimentosComEmojis } from '../diario/Sentimento'
 import { IDiario } from '../../services/GetUserDiarioByDate'
-import EmptyState from './EmptyState'
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    titulo: {
-      width: 303,
-      marginTop: 36,
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    }
-  })
-)
+import GraficoBase from './GraficoBase'
 
 interface IProps {
   diarios: Array<IDiario>
@@ -24,47 +9,18 @@ interface IProps {
 }
 
 const GraficoSentimentos: FC<IProps> = ({ diarios, mesAtual }) => {
-  const classes = useStyles()
   const sentimentosDoMes = diarios
     ?.map(diario => diario.sentimentos)
     .flat()
     .filter(sentimento => Boolean(sentimento))
 
-  if (sentimentosDoMes?.length === 0) {
-    return <EmptyState />
-  }
-
-  const sentimentos = [...new Set(sentimentosDoMes)]
-
-  const sentimentoAgrupados = sentimentosDoMes?.reduce((grupo, sentimento) => {
-    grupo[sentimento] = grupo[sentimento] ? grupo[sentimento] + 1 : 1
-
-    return grupo
-  }, {})
-
   return (
-    <Box>
-      <Typography className={classes.titulo}>
-        Acompanhe a frequência de cada emoção ao longo do mês:
-      </Typography>
-      <Box mt={2}>
-        {sentimentos.map((sentimento, index) => {
-          const quantidadeNoMes = sentimentoAgrupados[sentimento]
-          const percentual = (quantidadeNoMes * 100) / getDaysInMonth(mesAtual)
-          const emoji = listaDeSentimentos.find(
-            item => item.nome === sentimento
-          )
-          return (
-            <ProgressBar
-              key={index}
-              completed={percentual}
-              label={sentimento}
-              emoji={emoji.emoji}
-            />
-          )
-        })}
-      </Box>
-    </Box>
+    <GraficoBase
+      registrosDoMes={sentimentosDoMes}
+      mesAtual={mesAtual}
+      listaComEmojis={listaDeSentimentosComEmojis}
+      titulo="Acompanhe a frequência de cada emoção ao longo do mês:"
+    />
   )
 }
 
