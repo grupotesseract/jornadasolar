5e21e7cc73085e187f1c15b7675682f27600addc
kellynvd <kellynvd@gmail.com>
2021-04-07 17:53:34 -0300

    feat: Adicionar alerta para meditações
    - Ajusta função onClose no Alert
    - Adiciona actions de meditações para os alerts
    - Adiciona cases de alert para meditações

---
+++ src/redux/admin/meditacoes.ts
@@ -0,0 +1,6 @@
+import { createAction } from '@reduxjs/toolkit'
+
+export const createdOrUpdated = createAction('meditacoes/createdOrUpdated')
+export const deleteMeditacao = createAction('meditacoes/delete')
+
+export default {}
