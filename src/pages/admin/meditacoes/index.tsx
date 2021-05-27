ed51d18fb5f086142d6c93b5a5e14db47cb99925
Thais Maria <thais.map@hotmail.com>
2021-05-27 11:51:29 -0300

    feat: usa iconButtonConfirmacao
    - Troca o uso direto do Dialogo pelo uso do IconButtonConfirmacao para confirmar exclusão

--- src/pages/admin/meditacoes/index.tsx
+++ src/pages/admin/meditacoes/index.tsx
@@ -3,7 +3,7 @@ import Typography from '@material-ui/core/Typography'
 import Table from 'src/components/admin/Table'
 import GetAllMeditacoes from '../../../services/meditacoes/GetAllMeditacoes'
 import Button from 'src/components/Button'
-import { Box, IconButton, Grid, makeStyles } from '@material-ui/core'
+import { Box, Grid, makeStyles } from '@material-ui/core'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import DeleteIcon from '@material-ui/icons/Delete'
@@ -12,7 +12,7 @@ import AdminBase from 'src/components/templates/AdminBase'
 import { useDispatch } from 'react-redux'
 import { deleteMeditacao as deleteMeditacaoAction } from '../../../redux/admin/meditacoes'
 import { withAdmin } from 'src/components/hocs/withAuth'
-import Dialogo from 'src/components/Dialogo'
+import IconButtonConfirmacao from 'src/components/IconButtonConfirmacao'
 
 const useStyles = makeStyles({
   tituloContainer: {
@@ -36,8 +36,6 @@ const useStyles = makeStyles({
 
 const Meditacoes: FC = () => {
   const [meditacoes, setMeditacoes] = useState([])
-  const [isDialogoOpen, setIsDialogoOpen] = useState(false)
-  const [meditacaoSelecionada, setMeditacaoSelecionada] = useState('')
 
   const router = useRouter()
   const dispatch = useDispatch()
@@ -58,18 +56,8 @@ const Meditacoes: FC = () => {
     { id: 'actions', label: 'Ações' }
   ]
 
-  const abrirDialogo = id => {
-    setMeditacaoSelecionada(id)
-    setIsDialogoOpen(true)
-  }
-
-  const fecharDialogo = () => {
-    setMeditacaoSelecionada('')
-    setIsDialogoOpen(false)
-  }
-
-  const handleDelete = () => {
-    new DeleteMeditacao().call(meditacaoSelecionada).then(() => {
+  const handleDelete = id => {
+    new DeleteMeditacao().call(id).then(() => {
       dispatch(deleteMeditacaoAction())
       buscarMeditacoes()
     })
@@ -80,18 +68,18 @@ const Meditacoes: FC = () => {
     nome: meditacao.nome,
     data: meditacao.data,
     actions: (
-      <IconButton
-        style={{ padding: 0 }}
-        onClick={() => abrirDialogo(meditacao.id)}
-      >
-        <DeleteIcon />
-      </IconButton>
+      <IconButtonConfirmacao
+        icone={<DeleteIcon />}
+        onConfirmar={() => handleDelete(meditacao.id)}
+        conteudoDoDialogo="Você tem certeza que deseja excluir a meditação?"
+        tituloDoDialogo="Excluir meditação"
+        labelDeConfirmacaoDoDialogo="Excluir"
+      />
     ),
     onClick: () => {
       router.push(`/admin/meditacoes/${meditacao.id}`)
     }
   }))
-
   return (
     <AdminBase>
       <Grid item xs={12}>
@@ -108,15 +96,6 @@ const Meditacoes: FC = () => {
           </Link>
         </Box>
         <Table headCells={headCells} bodyCells={bodyCells} />
-        <Dialogo
-          isOpen={isDialogoOpen}
-          onConfirmar={handleDelete}
-          onFechar={fecharDialogo}
-          titulo="Excluir meditação"
-          labelConfirmar="Excluir"
-        >
-          Você tem certeza que deseja excluir a meditação?
-        </Dialogo>
       </Grid>
     </AdminBase>
   )
