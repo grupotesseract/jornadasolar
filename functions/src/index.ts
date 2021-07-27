f012298efb754fba28c5f6bf21efaf300df2999d
Thais <thais.marpi@gmail.com>
2021-07-27 19:40:49 -0300

    feat(migrar-registros): migração de sentimentos no diario
    Migração de todos os sentimentos nos registros, substituindo os nomes pelos ids
    
    Ref #178797785

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,21 @@
 /* eslint-disable quotes */
-import {createGruposDeHabitosModelos} from "./createGruposDeHabitosModelos";
-import {createSentimentosModelos} from "./createSentimentosModelos";
-import {preencheGruposDeHabitosUsers} from "./preencheGruposDeHabitosUsers";
-import {preencheSentimentosUsuarios} from "./preencheSentimentosUsuarios"
-import {migrarHabitosPersonalizados} from "./migrarHabitosPersonalizados";
-import {migrarRegistrosHabitos} from "./migrarRegistrosHabitos";
-import * as admin from "firebase-admin";
+import { createGruposDeHabitosModelos } from "./createGruposDeHabitosModelos"
+import { createSentimentosModelos } from "./createSentimentosModelos"
+import { preencheGruposDeHabitosUsers } from "./preencheGruposDeHabitosUsers"
+import { preencheSentimentosUsuarios } from "./preencheSentimentosUsuarios"
+import { migrarHabitosPersonalizados } from "./migrarHabitosPersonalizados"
+import { migrarRegistrosHabitos } from "./migrarRegistrosHabitos"
+import { migrarRegistrosSentimentos } from "./migrarRegistrosSentimentos"
+import * as admin from "firebase-admin"
 
 admin.initializeApp()
 
 export {
-  createGruposDeHabitosModelos, 
+  createGruposDeHabitosModelos,
   createSentimentosModelos,
   preencheGruposDeHabitosUsers,
-  preencheSentimentosUsuarios, 
+  preencheSentimentosUsuarios,
   migrarHabitosPersonalizados,
-  migrarRegistrosHabitos
+  migrarRegistrosHabitos,
+  migrarRegistrosSentimentos
 }
